d07194f2f049ecd0f1ca90f7a0af7b110b159088
Manu Kumar Yadav <manukumaryadav769@gmail.com>
2026-05-02 16:22:26 +0530

    Refactor server.js for cleaner MongoDB connection

--- server.js
+++ server.js
@@ -64,19 +64,18 @@ const createAdmin = async () => {
   }
 };
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(async () => {
+  .then(() => {
     console.log("MongoDB Connected Successfully");
-
-    await createAdmin();
-
-    const PORT = process.env.PORT || 5000;
-
-    app.listen(PORT, () => {
-      console.log("Server running on port " + PORT);
-    });
+    createAdmin();
   })
   .catch((err) => {
     console.log("DB Error:", err.message);
   });
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
